a7790a3080c27069a8588ed332b569bb38d454b6
Emiki <benheng02624@gmail.com>
2023-08-06 22:51:04 +0800

    readme
    a

---
+++ Game Engines 2302/Assets/test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("let me know when you see this message radian");
+        Debug.Log("let me know when you see this message danial");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
